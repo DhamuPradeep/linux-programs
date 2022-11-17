e50f9889f86f74c8604b771cf2e79eeebff1d2d4
Dhamu Pradeep <73932175+DhamuPradeep@users.noreply.github.com>
2022-11-17 10:42:16 +0530

    Create greatest of three.sh

---
+++ greatest of three.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "enter n1"
+read n1
+echo "enter n2"
+read n2
+echo "enter n3"
+read n3
+echo "The greater number is "
+if [ $n1 -gt $n2 ] && [ $n1 -gt $n3 ]
+then
+        echo $n1
+elif [ $n2 -gt $n1 ] && [$n2 -gt $n3 ]
+then
+        echo $n2
+else
+        echo $n3
+fi

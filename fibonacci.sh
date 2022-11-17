25200368ae01d6016be101977a5457c894115a32
Dhamu Pradeep <73932175+DhamuPradeep@users.noreply.github.com>
2022-11-17 10:41:16 +0530

    Create fibonacci.sh

---
+++ fibonacci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+                                                                                              
+read n
+                                                                                              
+a=0
+b=1
+                                                                                              
+for ((i=0;i<n;i++))
+do
+        echo "$a "
+        fn=$((a+b))
+        a=$b
+        b=$fn
+done         
